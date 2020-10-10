42d573f686443b97671fe80f6f5810913b9d911c
xkendx <63884610+xkendx@users.noreply.github.com>
2020-10-10 22:50:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={[styles.top, {flex:1}]}>
-        <View style={[styles.top_container, {flex:1, flexDirection:"row"}]}>
-          <View style={[styles.orange_circle,{flex:1} ]}>
+      <View style={[styles.top, {flex: 1}]}>
+        <View style={[styles.top_container, {flex: 1, flexDirection: 'row'}]}>
+          <View style={[styles.orange_circle, {flex: 1}]}>
             <Text style={styles.orange_circle_text}>Orange Circle</Text>
           </View>
-          <View style={[styles.yellow_box,{flex:1}]}>
+          <View style={[styles.yellow_box, {flex: 1}]}>
             <Text style={styles.yellow_box_text}>Yellow Box</Text>
           </View>
         </View>
       </View>
-      <View style={[styles.bottom, {flex: 2, flexDirection:"row"}]}>
-        <View style={[styles.black_box, {flex: 3,}]}>
-            <Text style={styles.black_box_text}>Black Box</Text>
-          </View>
-          <View style={[styles.brown_box, {flex:1}]}>
-            <Text style={[styles.brown_box_text,  {}]}>Brown</Text>
-            <Text style={[styles.brown_box_text,  {}]}>Box</Text>
-          </View>
+      <View style={[styles.bottom, {flex: 2, flexDirection: 'row'}]}>
+        <View style={[styles.black_box, {flex: 3}]}>
+          <Text style={styles.black_box_text}>Black Box</Text>
         </View>
+        <View style={[styles.brown_box, {flex: 1}]}>
+          <Text style={[styles.brown_box_text, {}]}>Brown</Text>
+          <Text style={[styles.brown_box_text, {}]}>Box</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -36,72 +31,72 @@ export default App;
 
 const styles = StyleSheet.create({
   top: {
-    backgroundColor:"red",
+    backgroundColor: 'red',
   },
   bottom: {
-    backgroundColor:"pink",
-},
+    backgroundColor: 'pink',
+  },
   top_container: {
-    backgroundColor:"gray",
+    backgroundColor: 'gray',
     margin: 20,
-    borderRadius:10,
-    alignItems: "center",
-    justifyContent:"center",
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   orange_circle: {
-    alignItems: "center",
-    justifyContent:"center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   orange_circle_text: {
-    backgroundColor:"orange",
-    width:100,
-    height:100,
-    borderRadius:100,
-    fontWeight:"bold",
+    backgroundColor: 'orange',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    fontWeight: 'bold',
     textAlign: 'center',
-    textAlignVertical:"center",
+    textAlignVertical: 'center',
   },
   yellow_box: {
-    alignItems: "center",
-    justifyContent:"center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   yellow_box_text: {
-    backgroundColor:"yellow",
-    borderRadius:20,
-    width:100,
-    height:50,
-    alignItems: "center",
+    backgroundColor: 'yellow',
+    borderRadius: 20,
+    width: 100,
+    height: 50,
+    alignItems: 'center',
     textAlign: 'center',
-    textAlignVertical:"center",
+    textAlignVertical: 'center',
   },
   black_box: {
-    flex:3,
-    backgroundColor:"black",
-    borderRadius:20,
+    flex: 3,
+    backgroundColor: 'black',
+    borderRadius: 20,
     margin: 30,
-    justifyContent:"center",
+    justifyContent: 'center',
   },
   black_box_text: {
-    color:"white",
-    fontWeight:"bold",
-    textAlign:"center",
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   brown_box: {
-    backgroundColor:"brown",
+    backgroundColor: 'brown',
     borderRadius: 80,
     margin: 20,
     // flex:1,
-    flexDirection:"row",
+    flexDirection: 'row',
   },
   brown_box_text: {
     // flex:1,
-    color:"white",
-    textAlign:'left',
-    transform: [{ rotate: '270deg'}],
-    textAlignVertical:"center",
-    fontSize:10,
-    alignSelf:"center",
-    justifyContent:"center",
-    alignItems:"center",
+    color: 'white',
+    textAlign: 'left',
+    transform: [{rotate: '270deg'}],
+    textAlignVertical: 'center',
+    fontSize: 10,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
